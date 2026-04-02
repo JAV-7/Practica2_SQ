3be9824f85bab86f7f94c1aceb0b6a70b72738c0
JAV-7 <fjavier.ramosj@gmail.com>
2026-04-02 10:07:43 -0600

    fix: set correct imports

--- src/test_random_coffee_machine.py
+++ src/test_random_coffee_machine.py
@@ -14,7 +14,7 @@ from unittest.mock import patch
 
 from machine_operations import PASSWORD, deposit_money, withdraw_money
 
-from src import (
+from coffee_machine import (
     MENU,
     create_coffee_machine,
     fill_the_machine,
